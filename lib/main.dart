28bc0ef44aea08eaddae70d847744354161c2403
ahmed farouk <faroukahmed804@gmail.com>
2025-07-26 15:47:40 +0300

    creat_note_icon

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class NotesApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(brightness: Brightness.light),
+      theme: ThemeData(brightness: Brightness.dark, fontFamily: 'Poppins'),
       home: const NotesView(),
     );
   }
---
+++ lib/widgets/custom_note_item.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class NoteIcon extends StatelessWidget {
+  const NoteIcon({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(top: 16, left: 24, bottom: 24),
+      decoration: BoxDecoration(
+        color: Colors.yellow,
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          ListTile(
+            title: Text(
+              'Flutter Note',
+              style: TextStyle(fontSize: 26, color: Colors.black),
+            ),
+            subtitle: Padding(
+              padding: const EdgeInsets.only(top: 16),
+              child: Text(
+                'write the Sub here..........................',
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.black.withAlpha(100),
+                ),
+              ),
+            ),
+            trailing: IconButton(
+              onPressed: () {},
+              icon: const Icon(FontAwesomeIcons.trash, color: Colors.black),
+            ),
+          ),
+
+          Padding(
+            padding: const EdgeInsets.only(right: 24, top: 16),
+            child: Text(
+              'The Date */*/*',
+              style: TextStyle(
+                fontSize: 16,
+                color: Colors.black.withAlpha(100),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
